fb232b9b6d0ca05f3c21f8c3d7b14e8a93853375
Batın Yaren <batinyaren1914@gmail.com>
2026-07-05 14:48:58 +0300

    main

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -7,16 +7,16 @@ const initializeFirebaseAdmin = () => {
 
     return admin.initializeApp({
         credential: admin.credential.cert({
-            projectId: process.env.FIREBASE_ADMIN_PROJECT_ID as string,
-            clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL as string,
-            privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n') as string,
+            projectId: "karsgc3",
+            clientEmail: "firebase-adminsdk-fbsvc@karsgc3.iam.gserviceaccount.com",
+            privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQC3qvQWCwEqBStc\n2rnx79GmpwWCJAfp8SR5M10zG8pWhvQ43DPrJxBZrQlrSCXWdCXVcrGjB4WXmJ1U\nHVycCmQe7HxCSxB4ux37NAruJ3qrklYpkTKkX7gECJEA1le/rBBQ0EkRI48Y0aE0\nrHBxev0WFMWu6OVYo5GjmOT6u6z7kSV/A/i33h31+/dSFUhu/S5lFVOGiAJzxAob\nz5JpNTPo8dQhzG23+o7meaH/TauxQLz1MWSOlU5DSEK24OWHp8reQbUeofmmgL0e\nVYquRAxDAHiJqqiDHIyEuwmbnSOz9DJa+029EQ6OcIr+izKeqLxS51a9ZstwQCQo\nMvGcwWoxAgMBAAECggEAFhI1lcaJw8ZuQRDJwpCapYexqkNxLgQ76VKqUv92b7J0\n+7Y+iPYfSV95X8qCPYSknUNUpG49eAuvcFG89MVs5ahugDrsBk4aGrtUyuy0mZSS\nxRj9OHKGNvnejvazAcYICeYVubkHs6hRUnacZo6shjBF1zCGiwHQ+RNhBN5zuaEx\n9C9LoA0nmAlXQsJeMsYwBxYChxTT8Pg/BYrjtAU+gYScailDctARctkRRkR1HVMd\nSIPvNajtJVlM+j/9bMYMjxNB0Z6AmJGcXQAPB3lfImtgPFlGBn9/IXXwyubU9M/8\nOP/QVVlbweSN0sJxZbpvrJGfZ7OyNH6nYaal/Cv/fwKBgQDhdYS8uDgf3OISilvF\nVfMvZHVJfUV3LAJ29kywtegefowwyQEGKfwuUwrxpcl9FvhLryofnzN627VhlIIe\nPu5NhVs5wOzpobgTSRr0rAozgTklffDactM5F6pYgOyAD2NS16hoZGxefCxGkzhc\nA8UeUUT6dNnm1sPKYEICdwxhZwKBgQDQjDHs4mJPhOBhkSSi9xciiemYpxjOSA8R\nrriv54HbKBTMU/IElorLS6BalpqEdS+zthoAaN5QN9tbXvZU1q22MizxGzjqaDZr\n9FLAzTZMz0jSn8Haru49gJEAIZPDKzkv9yxuPUy1CV2+rgyrVYsTCypAi5tP9dLB\nXgk6VtcgpwKBgQDC6CVoQJnEcd++x32uiy9AuUfr0DBNZe57iVGjMp8IPb5ULnCE\nM6efJItBfGFDkqf0RenUjHykqWuUgB48p4q72Z2TwAUPtI69nWWAP3pSLFSK5OVi\n9AFGSf8po8rQqnzjESy1kptc01wzZvJCGcA/BWLsYjg+J0v7mFt4ZhJ94QKBgQCs\nolE1wdUV9G319jt4t8gr7DQ2DKTJJfdrcNIrsx3YuF43IopmZ+QvE8SDGMaCPaum\nPWlay3S0vPhVs8+APVIBT5nKYPmFeY0LYm4acgWwIasrx9mmWEaZHNNrY2A5II+v\neBIMk6vq9PzoOpX8Qszkj8qDMc34rAqxzVEgPQDKYwKBgQCISCp3pGGHEADGdRdW\nRdzBo2wCxIU5NDxwMNkQOqM0RXYnqFo464MELmlakUpMagMlJxpSV4uyk0gVXKpE\nQToQQ4K20r9s2UNC0a9zRiOWZXpXBbz/STZy+nGJ4jkm07kEayhRH/6gIHcRdGrI\nvgOl3bY0UC9aqL39DzgCxNEifQ==\n-----END PRIVATE KEY-----\n",
         }),
         projectId: firebaseConfig.projectId,
         storageBucket: firebaseConfig.storageBucket
-    })
+    });
 }
 
-initializeFirebaseAdmin()
+/*initializeFirebaseAdmin()
 
 export const getApplicationStatistics = async () => {
     const firestore = getFirestore()
@@ -32,4 +32,4 @@ export const getApplicationStatistics = async () => {
         presses: presses + 20,
         chairs: chairs + 20
     }
-}
+}*/

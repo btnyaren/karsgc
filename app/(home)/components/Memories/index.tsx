09354fa45b719ce5e21ef840a986d6fc4a0a97c4
Batın Yaren <batinyaren1914@gmail.com>
2026-07-06 12:18:40 +0300

    main

--- app/(home)/components/Memories/index.tsx
+++ app/(home)/components/Memories/index.tsx
@@ -1,4 +1,5 @@
 import AnimationContainer from "@/components/AnimatonContainer";
+import PhotoRenderer from "./PhotoRenderer";
 
 const Memories = () => {
     return (
@@ -13,6 +14,7 @@ const Memories = () => {
             >
                 <h2 style={{fontSize: "clamp(16px, 4dvw, 30px)"}} className="pt-8 pb-6 sm:pt-11 sm:pb-12 text-center font-semibold [text-shadow:2px_2px_10px_black]">Kars{"'"}ın Gençlik Çalıştayı </h2>
             </AnimationContainer>
+            <PhotoRenderer />
         </section>
     );
 };
